259539ef40b6985765c8200a34da97874393e9fb
zwpdbh <hyperion_z@outlook.com>
2017-12-16 23:45:53 +1300

    < pthread_hellow_world !

---
+++ cmake-build-debug/CMakeFiles/learning_pthreads_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_pthreads_01.dir/Parallel_Computing/learning_pthreads/01/main.cpp.o"
+  "learning_pthreads_01.pdb"
+  "learning_pthreads_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning_pthreads_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
