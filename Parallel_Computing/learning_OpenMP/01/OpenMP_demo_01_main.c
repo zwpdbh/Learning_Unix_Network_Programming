619b95358325be60a2881b4806a405787df4c334
zwpdbh <hyperion_z@outlook.com>
2018-01-05 19:06:05 +1300

    > 3.3 Using OpenMP to Parallelize the Matrix Times Vector Prod- uct

--- Parallel_Computing/learning_OpenMP/01/OpenMP_demo_01_main.c
+++ Parallel_Computing/learning_OpenMP/01/OpenMP_demo_01_main.c
@@ -8,6 +8,13 @@
  *
  * The most straightforward way is to calculate the result vector a by computing the dot product of the rows of the matrix
  * and vector
+ *
+ * To execute this program, on terminal:
+ * ./bin/OpenMP_demo_01
+    Please give m and n for matrix's m row and n column:
+    100, 50
+    initializing matrix B and vector c
+    executing mxv function for m = 100, n = 50
  */
 
 #include <stdio.h>
@@ -18,7 +25,57 @@ void mxv(int m, int n, double* restrict a, double* restrict b, double* restrict 
 int main(int argc, char* argv[]) {
     double *a, *b, *c;
 
+
+    // m is the number of rows, n is the number of columns
     int i, j, m, n;
 
+    printf("Please give m and n for matrix's m row and n column: \n");
+    scanf("%d, %d",&m, &n);
+
+    if ((a = (double* )malloc(m * sizeof(double))) == NULL) {
+        perror("memory allocation for a");
+    }
+
+    if ((b = (double*) malloc(m * n * sizeof(double))) == NULL) {
+        perror("memory allocation for b");
+    }
+
+    if ((c = (double*) malloc(n * sizeof(double))) == NULL) {
+        perror("memory allocation for c");
+    }
+
+    printf("initializing matrix B and vector c\n");
+    for (j = 0; j < n; j++) {
+        c[j] = 2.0;
+    }
+
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            b[i * n + j] = i;
+        }
+    }
+
+    printf("executing mxv function for m = %d, n = %d\n", m, n);
+    (void) mxv(m, n, a, b, c);
+    free(a);
+    free(b);
+    free(c);
+
+
+    return 0;
+}
+
+
+void mxv(int m, int n, double* restrict a, double* restrict b, double* restrict c)
+{
+    int i, j;
+
+    for (i = 0; i < m; i++) {
+        a[i] = 0.0;
+
+        for (j = 0; j < n; j++) {
+            a[i] += b[i * n + j] * c[j];
+        }
+    }
 }
 
---
+++ cmake-build-debug/CMakeFiles/playground.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/playground.dir/playground_main.c.o"
+  "playground.pdb"
+  "playground"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/playground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
