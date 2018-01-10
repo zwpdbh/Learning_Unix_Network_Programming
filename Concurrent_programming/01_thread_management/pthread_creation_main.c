90251dcecb0d6aa7a8d92495ff36a7a5f6bd3bfe
zwpdbh <hyperion_z@outlook.com>
2018-01-10 20:58:38 +1300

    >

---
+++ Concurrent_programming/01_thread_management/pthread_creation_main.c
@@ -0,0 +1,13 @@
+//
+// Created by zwpdbh on 10/01/2018.
+//
+
+#include <pthread.h>
+#include "../apue.h"
+
+pthread_t ntid;
+
+void print_ids(const char* s) {
+    pid_t pid;
+    
+}
---
+++ cmake-build-debug/Concurrent_programming/01_thread_management/CMakeFiles/pthread_creation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pthread_creation.dir/main_pthread_creation.c.o"
+  "../../../bin/pthread_creation.pdb"
+  "../../../bin/pthread_creation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pthread_creation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
