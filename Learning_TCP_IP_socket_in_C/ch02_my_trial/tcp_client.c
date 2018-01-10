a580e56d84f68adbbea0e31a885fab419a780672
zwpdbh <hyperion_z@outlook.com>
2018-01-10 20:09:30 +1300

    > add ch02_my_trial for tcp_ip programming

---
+++ Learning_TCP_IP_socket_in_C/ch02_my_trial/tcp_client.c
@@ -0,0 +1,8 @@
+//
+// Created by zwpdbh on 10/01/2018.
+//
+
+int main() {
+
+    return 0;
+}
---
+++ cmake-build-debug/Learning_TCP_IP_socket_in_C/ch02_my_trial/CMakeFiles/tcp_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_client.dir/tcp_client.c.o"
+  "../../../bin/tcp_client.pdb"
+  "../../../bin/tcp_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tcp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
