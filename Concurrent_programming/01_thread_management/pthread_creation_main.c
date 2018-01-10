d2e00bbbf4b6fd99f9ff0139b777982091b4717f
zwpdbh <hyperion_z@outlook.com>
2018-01-10 21:44:30 +1300

    > C++ version of thread creation.

--- Concurrent_programming/01_thread_management/pthread_creation_main.c
+++ Concurrent_programming/01_thread_management/pthread_creation_main.c
@@ -20,18 +20,22 @@ void print_ids(const char* s) {
 }
 
 void* thread_func(void *arg) {
+
     print_ids("new thread: ");
     return (void* )0;
 }
 
 int main(void) {
     int err;
+
     err = pthread_create(&ntid, NULL, thread_func, NULL);
     if (err != 0) {
         perror("cannot create thread\n");
         exit(EXIT_FAILURE);
     }
+
     print_ids("main thread: ");
+
     sleep(1);
     exit(0);
 }
